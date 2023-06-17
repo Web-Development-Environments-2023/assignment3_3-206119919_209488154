127f44cba187af43ea0b003df6f0130d604738d7
Shaked-BA <shaked.ba3@gmail.com>
2023-06-17 20:32:25 +0300

    nav design

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link :to="{ name: 'main' }">Vue Recipes</router-link>|
+      <router-link :to="{ name: 'main' }">Sanji's Social Recipes</router-link>|
       <router-link :to="{ name: 'search' }">Search</router-link>|
       <!-- {{ !$root.store.username }} -->
       <span v-if="!$root.store.username">
-        Guest:
-        <router-link :to="{ name: 'register' }">Register</router-link>|
-        <router-link :to="{ name: 'login' }">Login</router-link>|
+        Guest: 
+        <router-link :to="{ name: 'register' }"> Register </router-link>|
+        <router-link :to="{ name: 'login' }"> Login </router-link>
       </span>
       <span v-else>
         {{ $root.store.username }}: <button @click="Logout">Logout</button>|
