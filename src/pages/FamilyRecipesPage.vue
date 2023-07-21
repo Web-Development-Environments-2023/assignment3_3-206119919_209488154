ee95645c30b5e18aa506ed7a2cce877020475b42
Shaked-BA <shaked.ba3@gmail.com>
2023-07-21 14:54:38 +0300

    Added family recipes page.

--- src/pages/FamilyRecipesPage.vue
+++ src/pages/FamilyRecipesPage.vue
@@ -1,6 +1,47 @@
 <template>
-    <div class="container">
-      <h1 class="title">Family Recipes Page</h1>
+  <div class="container">
+    <h1 class="title">Family Recipes Page</h1>
+    <div class="recipes">
+      <h1>Cuscous</h1>
+      <ul>
+        <li><strong>Whose recipe:</strong> Omer's Mom.</li>
+        <li><strong>Customary preparation:</strong> Cuscous is a common dish for fridays dinners.</li>
+        <li><strong>Components:</strong> carrots, onion, zucchini and garlic, fresh cilantro and mint, lemon, turmeric, cumin, cinnamon and coriander, olive oil, chicken broth, chickpeas, couscous.</li>
+        <li><strong>Method of preparation:</strong> Preheat oven to 200 degrees. Grease baking sheet. Add veggies, drizzle with oil and season. Roast until tender, about 15 minutes. Whisk olive oil, lemon, garlic and most spices. Bring chicken broth and turmeric to a boil. Pour hot chicken broth over couscous in bowl and toss. Cover and rest 5 minutes. Add roasted vegetables, chick peas, cilantro, mint and lemon mixture and toss.</li>
+      </ul>
+      <img src="../assets/couscous.jpg" alt="Couscous image" class="recipe-image">
+
+      <hr class="recipe-separator">
+
+      <h1>Kubbeh</h1>
+      <ul>
+        <li><strong>Whose recipe:</strong> Shaked's Grandmother.</li>
+        <li><strong>Customary preparation:</strong> Kubbeh Cuscous is a common dish for fridays dinners.</li>
+        <li><strong>Components:</strong> For the dough: 2 cups semolina, 1 Tbsp canola oil, 1 tsp salt, 1 cup lukewarm water.
+          For the filling: 1 Tbsp canola oil, 1 large onion, 1lb ground beef, ½ cup chopped parsley, 2 cloves garlic, 1 Tbsp baharat spice, 1 tsp salt, ¼ tsp black pepper.
+          For the soup: 5 Tbsp canola oil, 1 large onion, 4 cups chopped celery, 4 peeled beets, 6 cups chicken stock, 4 cups water, ½ cup lemon juice, 4 Tbsp sugar, 1½ tsp salt, ¾ tsp black pepper.</li>
+        <li><strong>Method of preparation:</strong> Start by making the dough. In a medium bowl mix the semolina, oil, and salt. Add the water and knead until smooth. Cover with plastic wrap and set aside.
+          Then make the filling. In a large sauté pan, heat the oil over medium heat. Sauté the onion until translucent. Add the meat and cook while breaking it apart, for several minutes. Add the parsley, garlic, baharat, salt, and pepper and cook until the meat is browned. Drain most of the oil from the pan and set aside to cool. 
+          Take the dough and pinch apart small balls about half the size of a golf ball. Roll it out into flat, thin rounds. Place a little less than a Tbsp meat in the center of the dough and bring the sides around it and pinch to seal. Pick it up and roll it in your hands a bit to form into a smooth ball. Place on a plate or baking sheet, cover with plastic wrap, and put in the refrigerator until ready to place in the soup.
+          Using a large soup pot so that the kubbeh do not stick together, heat the canola oil over medium heat and then sauté the onion until translucent but not browned. Add the beets and celery and mix. Add the chicken stock and water and bring to a boil. Add the lemon juice, sugar, salt, and pepper. Cook for about 8-10 minutes. Gently add the kubbeh to the pot, reduce the heat to a simmer and cook for 45 minutes-1 hour.</li>
+      </ul>
+      <img src="../assets/kubbeh.jpg" alt="Kubbeh image" class="recipe-image">
+
+      <hr class="recipe-separator">
+
+      <h1>Jachnun</h1>
+      <ul>
+        <li><strong>Whose recipe:</strong> Shaked's uncle.</li>
+        <li><strong>Customary preparation:</strong> Jachnun is a common dish for Saturdays breakfasts.</li>
+        <li><strong>Components:</strong> 8 cups all-purpose flour, ¼ cup sugar, ½ teaspoon baking powder, 2 tablespoons honey, 1 tablespoon salt, 2½ cups warm water, 1 tablespoon olive oil, ½ cup butter or olive oil.</li>
+        <li><strong>Method of preparation:</strong> Place flour, sugar, baking powder, honey, and salt in a large bowl. Add water and stir until the dough is shaggy and the water has been absorbed. Knead the dough in the bowl for 2 minutes. Set the dough aside at room temperature to rest for 5 minutes. Stretch and fold the dough a few times until the dough feels nice and smooth. Cover with a towel and set aside for 1 hour. Lightly oil a large container or glass casserole.  Oil your hands and coat the dough with a thin layer of oil. Separate the dough into 10 balls. Roll each one into a tight ball and place in oiled container. Cover with a towel and let rest 5 minutes. Preheat oven to 100 degrees. Line a deep pot with parchment paper and lightly oil. For each ball generously coat with oil and flatten as thin as possible with the palm of your hands. Spread butter or olive oil over top. Then beginning folding and rolling. Fold the left side over towards the center and then the right side. From the narrow edge slowly roll up the dough into a tight cylinder. Place in prepared pot in a single layer, top with parchment then add the second layer on top, across in a crosshatch pattern, repeat with a third layer. Top with parchment and layer of raw eggs. Cover and seal pot tightly. Place in oven and bake for 12 hours. The next morning remove from oven and serve with eggs, pickles and tomato dip.</li>
+      </ul>
+      <img src="../assets/jachnun.jpg" alt="Jachnun image" class="recipe-image">
     </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+@import "@/scss/familyRecipesPage-style.scss";
+</style>
   
